578afb45239310ddafc2f241552ca83ab35ac624
unknown <luan.diniz.moraes@hotmail.com>
2022-04-21 18:06:06 -0300

    Adicionado membro2 (Luan)

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, meu nome é Luan Diniz, tenho 20 anos, gosto muito de música,jogar e aprender coisas novas. Tenho pouca experiência prévia com programação e espero aprofundar meus conhecimentos em POO nessa disciplina." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
