7202ea8763aabaf3c06b2dc9dc9c3df03e9c2f46
ClaudioGero <93171295+ClaudioGero@users.noreply.github.com>
2022-04-26 00:51:02 -0300

    Add files via upload

---
+++ membro4.py
@@ -0,0 +1,2 @@
+def hello():
+    return "Oi, me chamo Claudio Gerolimetto, tenho 21 anos recém completados e meus hoobies incluem jogar jogos, sair com amigos e ir á academia. Tenho pouca experiência com programação pois começei a estudar sobre a partir do momento em que entrei no curso e espero que em POO2 eu consiga aprofundar meus conhecimentos em Python."
